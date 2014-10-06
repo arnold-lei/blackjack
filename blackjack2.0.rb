1f4534de9f3b92e26b8e670a0b49aede05466fe1
Arnold Lei <arnold.lei88@gmail.com>
2014-10-06 10:25:33 -0400

    New version of blackjack

--- blackjack2.0.rb
+++ blackjack2.0.rb
@@ -1,13 +1,104 @@
+require 'pry'
+
 #This is my second attempt at BlackJack after watching some of the solutions video. I think it might just be easier to restart
 #then to salvage my code as is. 
+#Methods!
+def calculating_totals(cards)
+	#isolate the rankings of the cards from the suits
+	value = cards.map {|e| e[1] }
 
+	total = 0 
+	value.each do |card|
+		if card == "A"
+			total += 11
+		elsif card.to_i == 0
+			total += 10
+		else
+			total += card.to_i
+		end
+	end
+
+	value.select{|card| card == 'A'}.count.times do 
+		total -= 10 if total > 21
+	end
+	total 
+end
+
+#Deal
+def deal(deck, hand)
+	hand << deck.pop
+end
 
 #Welcome to Black Jack
 puts "Welcome to Black Jack"
+puts "Welcome to my table, what's your name?"
+player_name = gets.chomp
+puts "Hi #{player_name} good luck!"
 
 #Creating the Deck
 ranking = %w(1 2 3 4 5 6 7 8 9 10)
 suits = %w(♦ ♣ ♥ ♠)
-deck = suits.product(ranks)
+deck = suits.product(ranking)
+deck.shuffle!
+
+#Dealing Cards
+player_hand = []
+dealer_hand = []
 
 
+player_hand << deck.pop
+dealer_hand << deck.pop
+player_hand << deck.pop
+dealer_hand << deck.pop
+
+dealer_total = calculating_totals(dealer_hand)
+player_total = calculating_totals(player_hand)
+
+puts "Dealer has: #{dealer_hand[0]} and #{dealer_hand[1]}, for a total of #{dealer_total}"
+puts "You have: #{player_hand[0]} and #{player_hand[1]}, for a total of: #{player_total}"
+
+#Player's Hit stay phase
+
+if player_total == 21
+	puts "Congratulation you won!"
+	exit 
+end 
+
+while player_total < 21 do 
+	puts "What would you like to do? Type 'H' to Hit, type 'S' to Stay"
+	choice = gets.chomp.to_s.upcase!
+	if choice == 'H'
+		deal(deck, player_hand)
+	elsif choice == 'S'
+		puts "You choose to stay."
+		break
+	else
+		puts "Please choose 'H' or 'S'"
+	end
+	player_total = calculating_totals(player_hand)
+	#not sure why I have to recalculate this again. 
+	puts "You have #{player_hand} for a total of #{player_total}"
+end
+
+if player_total > 21 
+	puts "Sorry, you busted"
+	exit 
+elsif player_total == 21
+	puts "Congratulation you won!"
+	exit 
+end
+
+#Dealer's Turn
+
+while dealer_total < 16
+	dealer_total = calculating_totals(dealer_hand)
+	deal(deck, dealer_hand)
+end
+
+if dealer_total > 21
+	puts "Congratualtions Dealer busted, you win!"
+elsif dealer_total > player_total
+	puts "Dealer has #{dealer_total}, you have #{player_total} sorry you lose"
+elsif player_total > dealer_total
+	puts "Dealer has #{dealer_total}, you have #{player_total} Congratualtions you win"
+end
--- test.rb
+++ test.rb
@@ -1,175 +1,160 @@
-require 'pry'
-#There are 52 Cards in a deck 
-#13 cards of each suit
-#cards go 1-10,J,Q,K,A
-# ♦, ♥, ♣, ♠
-#J,Q,K are worth 10 points each
-#A are worth 11 or 1 point
-
-#card = [{A =>[1,11]}], [{2 => [2]}], 3 =>[3]....}
-
-#Welcome to the game,
-#Ask player to enter their name
-#pass out the Cards. The order should go Player, Dealer, Player, Dealer
-#	2 cards to the player 
-#	2 cards to the dealer
-
-#Loop
-#	Dealer asks Player if he would like to Hit/Stay?
-#	Player chooses to Hit or stay
-#		if Hit player will then recieve another cards
-#end until player says Stay
-
-#Dealer receives Cards
-#	if he has >= 16 
-#	Dealer stays 
-
-#Compare card totals 
-#Highest score wins
-#Tie if values are the same 
-#Ask player to play again
-
-#Building the deck\
+# Interactive command line blackjack game
 
 
+def calculate_total(cards) 
+  # [['H', '3'], ['S', 'Q'], ... ]
+  arr = cards.map{|e| e[1] }
 
-def full_deck
-	ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
-	#suits = %w(diamonds clubs hearts spades)
-	suits = ['♦', '♣', '♥', '♠']
-	deck = suits.product(ranks)
+  total = 0
+  arr.each do |value|
+    if value == "A"
+      total += 11
+    elsif value.to_i == 0 # J, Q, K
+      total += 10
+    else
+      total += value.to_i
+    end
+  end
 
+  #correct for Aces
+  arr.select{|e| e == "A"}.count.times do
+    total -= 10 if total > 21
+  end
+
+  total
+end1
+
+# Start Game
+
+puts "Welcome to Blackjack!"
+
+suits = ['H', 'D', 'S', 'C']
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+deck = suits.product(cards)
+deck.shuffle!
+
+# Deal Cards
+
+mycards = []
+dealercards = []
+
+mycards << deck.pop
+dealercards << deck.pop
+mycards << deck.pop
+dealercards << deck.pop
+
+dealertotal = calculate_total(dealercards)
+mytotal = calculate_total(mycards)
+
+# Show Cards
+
+puts "Dealer has: #{dealercards[0]} and #{dealercards[1]}, for a total of #{dealertotal}"
+puts "You have: #{mycards[0]} and #{mycards[1]}, for a total of: #{mytotal}"
+puts ""
+
+# Player turn
+if mytotal == 21
+  puts "Congratulations, you hit blackjack! You win!"
+  exit
 end
 
-#Player & Dealer hands
-player_hand = []
-dealer_hand = []
+while mytotal < 21
+  puts "What would you like to do? 1) hit 2) stay"
+  hit_or_stay = gets.chomp
 
+  if !['1', '2'].include?(hit_or_stay)
+    puts "Error: you must enter 1 or 2"
+    next
+  end
 
+  if hit_or_stay == "2"
+    puts "You chose to stay."
+    break
+  end
 
-#Counter used to keep track of which cards are still in play
-card_counter = [*1..52]
+  #hit
+  new_card = deck.pop
+  puts "Dealing card to player: #{new_card}"
+  mycards << new_card
+  mytotal = calculate_total(mycards)
+  puts "Your total is now: #{mytotal}"
 
-#dealing cards to player
-def deal(deck, player_hand, card_counter)
-	#draw a random card from the deck
-	pick = card_counter.sample
-	random_card = deck[pick]
-	player_hand << random_card
-	puts "You have recieved a #{random_card[1]}#{random_card[0]}"
-	deck.delete_at(pick)
-	card_counter.delete_at(pick)
+  if mytotal == 21
+    puts "Congratulations, you hit blackjack! You win!"
+    exit
+  elsif mytotal > 21
+    puts "Sorry, it looks like you busted!"
+    exit
+  end
 end
 
-#Dealing Cards to dealer
-def deal_dealer(deck, dealer_hand, card_counter)
-	#draw a random card from the deck
-	pick = card_counter.sample
-	random_card = deck[pick]
-	dealer_hand << random_card
-	puts "The dealer recieved a #{random_card[1]}#{random_card[0]}"
-	deck.delete_at(pick)
-	card_counter.delete_at(pick)
+# Dealer turn
+
+if dealertotal == 21
+  puts "Sorry, dealer hit blackjack. You lose."
+  exit
 end
 
+while dealertotal < 17
+  #hit
+  new_card = deck.pop
+  puts "Dealing new card for dealer: #{new_card}"
+  dealercards << new_card
+  dealertotal = calculate_total(dealercards)
+  puts "Dealer total is now: #{dealertotal}"
 
-
-#Adding Points Condition
-def adding_points(hand)
-	choice = 0
-	value = hand.collect{|value| value[1]}
-	value.each do |points|
-		sum = 0
-		if points == 'K' || points == 'Q' || points == 'J'
-			points = 10
-		elsif points == 'A'
-			puts "What value do you want your Ace to be? 1 or 11"
-			choice = gets.chomp.to_i
-			loop do 
-				if choice == 1
-					points = 1
-				elsif choice == 11
-					points = 11
-				else
-					puts "please choose 1 or 11"
-				end
-			end until choice == 1 || choice == 11
-		else
-		end	
-		sum += points 
-		return sum
-	end
+  if dealertotal == 21
+    puts "Sorry, dealer hit blackjack. You lose."
+    exit
+  elsif dealertotal > 21
+    puts "Congratulations, dealer busted! You win!"
+    exit
+  end
 end
 
-player_score = adding_points(player_hand)
-dealer_score = adding_points(dealer_hand)
+# Compare hands
 
-#Evaluating Winner
+puts "Dealer's cards: "
+dealercards.each do |card|
+  puts "=> #{card}"
+end
+puts ""
 
-def winner(player_score, dealer_score)
-	if player_score > 21
-		puts "Bust, dealer wins"
-	elsif dealer_score > 21
-		puts "Bust #{player_name} wins!"
-	elsif player_score > dealer_score
-		puts "#{player_name}, you win!"
-	elsif player_score > dealer_score
-		puts "Too bad #{player_name}, you lose, try again."
-	end
+puts "Your cards:"
+mycards.each do |card|
+  puts "=> #{card}"
+end
+puts ""
+
+if dealertotal > mytotal
+  puts "Sorry, dealer wins."
+elsif dealertotal < mytotal
+  puts "Congratulations, you win!"
+else
+  puts "It's a tie!"
 end
 
-deck = full_deck
-
-puts "Welcome to my table, what's your name?"
-player_name = gets.chomp
-puts "Hi #{player_name} good luck!"
-
-#Deal Player's first card
-deal(deck, player_hand, card_counter)
-
-#Deal Computer's First Card
-deal_dealer(deck, dealer_hand, card_counter)
-
-#Deal Player's Second Card
-deal(deck, player_hand, card_counter)
-
-#Deal Computer's Second Card
-deal_dealer(deck, dealer_hand, card_counter)
-puts "Dealer has #{dealer_hand}" 
-#Dealer's total score
-adding_points(dealer_hand)
-#Player's hand
-puts "#{player_name} have #{player_hand}" 
-#Player's score
-adding_points(player_hand)
-
-#Player's Hit stay phase
-puts "Would you like to hit(h) or stay(s)"
-choice = gets.chomp.to_s
-deal(deck, player_hand, card_counter)
-loop do
-	puts "Would you like to hit(h) or stay(s)"
-	choice = gets.chomp.to_s
-	if choice == 'h'
-		deal(deck, player_hand, card_counter)
-		puts "#{player_name} you currently have #{player_hand}"
-		adding_points(player_hand)
-	else
-		break
-	end
-end until choice == 's'
+exit
 
 
-#Dealer's Hit Stay Phase
-loop do
-	puts "Dealer currently has #{dealer_score}"
-	if dealer_score < 17
-		deal_dealer(deck, dealer_hand, card_counter)
-	else 
-		break
-	end
-end
 
-winner(player_score, dealer_score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
